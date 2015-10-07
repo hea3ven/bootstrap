14c4dffb0262bc2ee08123a76c7e98d3ebb6abd8
Hea3veN <hea3venmc@gmail.com>
2015-10-07 20:36:59 -0300

    Add version and manage multiple mods usage

--- com/hea3ven/tools/bootstrap/Bootstrap.java
+++ com/hea3ven/tools/bootstrap/Bootstrap.java
@@ -5,17 +5,35 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 import com.google.common.base.Throwables;
+import com.google.common.collect.Maps;
 
 import net.minecraft.launchwrapper.Launch;
 
 public class Bootstrap {
 
-	protected static Path getLibsDir() {
+	public static String version = "1.0.0";
+
+	private static Map<String, String> loadedLibs = Maps.newHashMap();
+
+	public static void require(String modId, String versionPattern) {
+		if (!matches(versionPattern, version))
+			throw new RuntimeException("Mod '" + modId + "' requires Boostrap version "
+					+ versionPattern + ", but the current loaded version is " + version);
+	}
+
+	private static boolean matches(String versionPattern, String version) {
+		return Pattern.matches(Pattern.quote(versionPattern).replace("x", "\\E[0-9]+\\Q"), version);
+	}
+
+	private static Path getLibsDir() {
 		URL jarUrl = Bootstrap.class.getProtectionDomain().getCodeSource().getLocation();
 		if (!jarUrl.getProtocol().equals("jar"))
 			return null;
@@ -31,7 +49,17 @@ public class Bootstrap {
 		return jarDir.resolve("h3ntlibs");
 	}
 
-	public static void initLib(String name, String version) {
+	public static void initLib(String modId, String name, String version, String versionPattern) {
+		// Check if the library is already loaded
+		if (loadedLibs.containsKey(name)) {
+			if (matches(versionPattern, loadedLibs.get(name)))
+				return;
+			else
+				throw new RuntimeException("Mod '" + modId + "' requires library " + name
+						+ " with version " + versionPattern + " but the version "
+						+ loadedLibs.get(name) + " is loaded");
+		}
+
 		Path libsDir = getLibsDir();
 		if (libsDir == null)
 			return;
@@ -41,18 +69,30 @@ public class Bootstrap {
 			if (!Files.exists(libsDir))
 				Files.createDirectory(libsDir);
 
-			// TODO: remove old versions
+			if (!Files.exists(libsDir.resolve(libJarName))) {
+				// Remove other versions
+				try (DirectoryStream<Path> dirStream = Files.newDirectoryStream(libsDir)) {
+					for (Path path : dirStream) {
+						if (path.startsWith(name + "-") && path.endsWith(".jar")) {
+							Files.delete(path);
+						}
+					}
+				}
 
-			if (!Files.exists(libsDir.resolve(libJarName)))
 				Files.copy(Bootstrap.class.getResourceAsStream("/libs/" + libJarName),
 						libsDir.resolve(libJarName));
+			}
 		} catch (IOException e) {
-			Throwables.propagate(e);
+			throw new RuntimeException("Failed to extract library " + name + "-" + version
+					+ " for mod '" + modId + "'", e);
 		}
 		try {
 			Launch.classLoader.addURL(libsDir.resolve(libJarName).toUri().toURL());
+			loadedLibs.put(name, version);
 		} catch (MalformedURLException e) {
-			Throwables.propagate(e);
+			throw new RuntimeException(
+					"Failed to load library " + name + "-" + version + " for mod '" + modId + "'",
+					e);
 		}
 	}
 }
